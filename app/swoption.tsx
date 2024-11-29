6e5149f9eb703e07ff380874137954d6d478ebc2
Wonyoung Cho <skywalker721@Wonyoungs-MacBook-Pro.local>
2024-11-29 14:22:59 +0900

    opt

---
+++ app/swoption.tsx
@@ -0,0 +1,9 @@
+export const PackageList = [
+    { name: 'HPackage_01', price: 35510000 },
+    { name: 'Premium_02', price: 39910000 },
+    { name: 'SemiPackage_03', price: 43560000 },
+    { name: 'LimousineColor_04', price: 44590000 },
+    { name: 'Limousine_05', price: 44590000 },
+    { name: 'Family_06', price: 44590000 },
+    { name: 'Daily_07', price: 44590000 }
+]
---
+++ components/navbar.tsx
@@ -0,0 +1,59 @@
+import {
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarMenuItem
+} from '@nextui-org/navbar';
+import { Link } from '@nextui-org/link';
+import NextLink from 'next/link';
+
+import { siteConfig } from '@/config/site';
+
+export const Navbar = () => {
+  return (
+    <NextUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="center">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <img height="100" src="../image/SW-logo.png" width="150" />
+            <p className="font-bold text-inherit" style={{ fontSize: '2rem' }}>
+              &nbsp;&nbsp;&nbsp;서원모터스 견적서
+            </p>
+          </NextLink>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      />
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                  index === 2
+                    ? 'primary'
+                    : index === siteConfig.navMenuItems.length - 1
+                      ? 'danger'
+                      : 'foreground'
+                }
+                href="#"
+                size="lg"
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+    </NextUINavbar>
+  );
+};
