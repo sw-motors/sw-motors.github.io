c19a8d86fff25555003e17e160205926fb807e2a
sw-motors <seowonseat@naver.com>
2024-12-02 22:31:42 +0900

    f

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    output: 'export',
-    basePath: 'https://sw-motors.github.io/sw-motors-invoice'
-}
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
- 
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/sw-motors-invoice' : '',
+  basePath:  isProd ? "/sw-motors-invoice" : '',
+  output: "export"
+};
 
 export default nextConfig;
